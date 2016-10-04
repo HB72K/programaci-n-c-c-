e5ca62246df67dbaefa9813b03fc5aca23bca34c
Jose Angel ü Asií Nomas <ghettoxx2@gmail.com>
2016-10-03 19:50:50 -0500

    Create holamundo.c

---
+++ c/holamundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//incluye informacion acerca de la biblioteca estandar
+main ()       //define una funciion llamnda main que no recibe valores de argumentos
+{   //las pocisiones de main estan encerradas entre llaves
+printf("hola, mundo\n");  //main llama a la funicion de biblioteca printf. para escribir esta secuencia de caracteres; \n representa el caracter nueva linea
+}
